480818fe6f5f2a9e0e531e0d566a8869fffd0dfe
Reyan Ahtesham <reyan.ahtesham@wamolabs.com>
2026-07-15 00:41:46 +0500

    Adding Tag Table

--- backend/repository/project.py
+++ backend/repository/project.py
@@ -2,16 +2,14 @@ from __future__ import annotations
 from sqlalchemy import select
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
-from schema.project import User as db_project
+from schema.project import Project as db_project
 from models.project_models import Project,CreateProject
-from fastapi import HTTPException
-from helper_functions.hashing import check_password
 
 class ProjectRepo:
     @staticmethod
     async def create_project(data: Project, session: AsyncSession):
-        project=Project(id=data.id,name=data.name,start_date=data.start_date,end_date=data.end_date,archived=data.archived
-                        ,tag=data.tag,category=data.category,status=data.status,soft_delete=data.soft_delete)        
+        project=db_project(id=data.id,name=data.name,start_date=data.start_date,end_date=data.end_date,archived=data.archived
+                        ,category=data.category,status=data.status,soft_delete=data.soft_delete)        
         try:
             session.add(project)
             await session.commit()
