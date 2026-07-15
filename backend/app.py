b64799a2f2abdf67ea4d91e416e04babc2967d53
Reyan Ahtesham <reyan.ahtesham@wamolabs.com>
2026-07-15 17:14:25 +0500

    Added Prefixes to Routers

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,13 @@
 from router.authentication import router
+from router.project import router_project
+from router.task import router_task
 from fastapi import FastAPI
 
 app = FastAPI()
 
 app.include_router(router)
+app.include_router(router_project, prefix="/project")
+app.include_router(router_task, prefix="/task")
 
 
 
--- backend/router/task.py
+++ backend/router/task.py
@@ -6,33 +6,33 @@ from services.user_services import UserAuthenticationServices
 from dependencies.database import get_db
 from models.task_models import TaskCreation,Task,TaskUpdate
 from repository.task import TaskCrud
-router = APIRouter()
+router_task = APIRouter()
 
 
-@router.post("/create_task/")
+@router_task.post("/create_task/")
 async def create_task_route(task_data: TaskCreation, session: AsyncSession = Depends(get_db)):
     #roles based access and jwt auth in other branch. Will be added after
     task=await TaskCrud.add_task(task_data,session)
     return task
 
-@router.delete("/delete_task/")
+@router_task.delete("/delete_task/")
 async def delete_task_route(task_id: str, session: AsyncSession = Depends(get_db)):
     #roles based access and jwt auth in other branch. Will be added after
     await TaskCrud.delete_task(task_id,session)
     return "Task Deleted Successfully"
 
-@router.get("/view_task/")
+@router_task.get("/view_task/")
 async def view_task_route(session: AsyncSession = Depends(get_db)):
     #roles based access and jwt auth in other branch. Will be added after
     return await TaskCrud.get_all_tasks(session)
 
-@router.put("/update_task/")
+@router_task.put("/update_task/")
 async def update_task_route(user_data: TaskUpdate, session: AsyncSession = Depends(get_db)):
     #roles based access and jwt auth in other branch. Will be added after
     await TaskCrud.update_task(user_data,session)
     return "Updated"
 
-@router.put("/add_prerequisite/")
+@router_task.put("/add_prerequisite/")
 async def add_prerequisite_route(prerequisite_id: str, dependant_id: str, session: AsyncSession = Depends(get_db)):
     #roles based access and jwt auth in other branch. Will be added after
     await TaskCrud.add_prerequisite(prerequisite_id,dependant_id,session)
