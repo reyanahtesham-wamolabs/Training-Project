b28a436ccd372254c994740c48dae841855883c3
Reyan Ahtesham <reyan.ahtesham@wamolabs.com>
2026-07-15 19:12:46 +0500

    initial

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 from router.user_management import router_user_management
 app = FastAPI()
 
+#This will contain all the task and project functionality
+
 app.include_router(router)
 app.include_router(router_user_management)
 
