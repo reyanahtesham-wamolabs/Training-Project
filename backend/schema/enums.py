96468488aa6b8f833526e350e63765b3351e5a12
Reyan Ahtesham <reyan.ahtesham@wamolabs.com>
2026-07-17 22:54:55 +0500

    fix:Enum bug fix

--- backend/schema/enums.py
+++ backend/schema/enums.py
@@ -23,3 +23,8 @@ class Categories(Enum):
 class AssignmentRole(Enum):
     project_admin="project_admin"
     project_member="project_member"
+class OTPAction(Enum):
+    verify_profile="verify_profile"
+    change_password="change_password"
+    change_email="change_email"
+    change_name="change_name"
